c0293b969b66b2a927cf3bf9cb92b18a34e51835
Aaron <aarone0621@gmail.com>
2025-05-23 00:23:41 -0500

    added Scanner and Token files. ended at 4.5.2

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -33,7 +33,7 @@ public class Lox {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
-        for (; ; ) {
+        for (; ;) {
             System.out.print("> ");
             String line = reader.readLine();
             if (line == null) break;
